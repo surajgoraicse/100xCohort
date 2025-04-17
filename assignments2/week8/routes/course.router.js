b7a6ddb7ef89a10594f267af8f34481f000d1fbc
surajgoraicse <surajgoraicse@gmail.com>
2025-04-17 16:49:54 +0530

    course selling app

---
+++ assignments2/week8/routes/course.router.js
@@ -0,0 +1,17 @@
+const express = require('express')
+
+const app = express.Router()
+
+
+
+// user course routes
+app.get('/' , (req,res)=>{})
+
+
+
+// admin course routes
+app.get()
+
+
+
+module.exports = app
---
+++ assignments2/week8/routes/user.router.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+
+
+app.post('/signup', (req, res) => {
+})
+
+app.post('/signin', (req, res) => [
+    
+])
+
+
+
+
+module.exports = app
