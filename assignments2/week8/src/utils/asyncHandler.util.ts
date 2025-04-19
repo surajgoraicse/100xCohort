2b51d51d4a43d29747227e8cde89f01768329fab
surajgoraicse <surajgoraicse@gmail.com>
2025-04-19 08:42:33 +0530

    deleted files

--- assignments2/week8/src/utils/asyncHandler.util.ts
+++
@@ -1,9 +0,0 @@
-import { NextFunction, Request, Response } from "express";
-
-const asyncHandler = (fn) => {
-	return (req: Request, res: Response, next: NextFunction) => {
-		fn(req, res).catch((err: Error) => next(err));
-	};
-};
-
-export default asyncHandler
--- assignments2/week8/src/utils/types.ts
+++
@@ -1,9 +0,0 @@
-import { NextFunction, Request, Response } from "express";
-
-export type AsyncFn = (
-	req: Request,
-	res: Response,
-	next: NextFunction
-) => Promise<any>;
-
-
