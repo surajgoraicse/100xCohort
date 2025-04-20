9757440cbc54051f359bce3dd96ad74657e740cf
surajgoraicse <surajgoraicse@gmail.com>
2025-04-20 06:18:28 +0530

    course selling backend

---
+++ assignments2/week8-course-selling/src/routes/admin.ts
@@ -0,0 +1,5 @@
+import { Request, Response, Router } from "express";
+
+const app = Router();
+
+app.get("/", (req: Request, res: Response) => {});
--- assignments2/week8-course-selling/src/utils/ApiError.ts
+++ assignments2/week8-course-selling/src/utils/ApiError.ts
@@ -14,3 +14,6 @@ class ApiError extends Error {
 		}
 	}
 }
+
+
+export default ApiError
